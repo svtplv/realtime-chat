5f72c3c7bb86af68ce17b1aa4d8d9538628e05e6
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-05-26 02:35:51 +0300

    Add group messaging

--- realtime-chat/chat/consumers.py
+++ realtime-chat/chat/consumers.py
@@ -1,4 +1,6 @@
 import json
+
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
@@ -13,7 +15,15 @@ class ChatroomConsumer(WebsocketConsumer):
         self.chatroom = get_object_or_404(
             ChatGroup, group_name=self.chatroom_name
         )
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name, self.channel_name
+        )
         self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -22,6 +32,12 @@ class ChatroomConsumer(WebsocketConsumer):
         message = GroupMessage.objects.create(
             body=body, author=self.user, group=self.chatroom
         )
+        event = {"type": "message_handler", "message_id": message.id}
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+    def message_handler(self, event):
+        message_id = event["message_id"]
+        message = GroupMessage.objects.get(id=message_id)
         context = {"message": message, "user": self.user}
 
         html = render_to_string(
