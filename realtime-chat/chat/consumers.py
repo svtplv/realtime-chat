3623e9371fed5b280207b8add4781875cc64848a
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-05-26 06:22:23 +0300

    Remove commented code

--- realtime-chat/chat/consumers.py
+++ realtime-chat/chat/consumers.py
@@ -1,52 +1,10 @@
 import json
 
-from asgiref.sync import async_to_sync, sync_to_async
-from channels.generic.websocket import (
-    WebsocketConsumer,
-    AsyncWebsocketConsumer,
-)
+from channels.generic.websocket import (AsyncWebsocketConsumer,)
 from django.shortcuts import aget_object_or_404
 from django.template.loader import render_to_string
 
 from .models import ChatGroup, GroupMessage
-
-
-# class ChatroomConsumer(WebsocketConsumer):
-#     def connect(self):
-#         self.user = self.scope["user"]
-#         self.chatroom_name = self.scope["url_route"]["kwargs"]["chatroom_name"]
-#         self.chatroom = get_object_or_404(
-#             ChatGroup, group_name=self.chatroom_name
-#         )
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.chatroom_name, self.channel_name
-#         )
-#         self.accept()
-
-#     def disconnect(self, close_code):
-#         async_to_sync(self.channel_layer.group_discard)(
-#             self.chatroom_name, self.channel_name
-#         )
-
-#     def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         body = text_data_json["body"]
-
-#         message = GroupMessage.objects.create(
-#             body=body, author=self.user, group=self.chatroom
-#         )
-#         event = {"type": "message_handler", "message_id": message.id}
-#         async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
-
-#     def message_handler(self, event):
-#         message_id = event["message_id"]
-#         message = GroupMessage.objects.get(id=message_id)
-#         context = {"message": message, "user": self.user}
-
-#         html = render_to_string(
-#             "chat/partials/chat_message_p.html", context=context
-#         )
-#         self.send(text_data=html)
 
 
 class ChatroomConsumer(AsyncWebsocketConsumer):
@@ -78,10 +36,11 @@ class ChatroomConsumer(AsyncWebsocketConsumer):
 
     async def message_handler(self, event):
         message_id = event["message_id"]
-        message = await GroupMessage.objects.aget(id=message_id)
+        message = await GroupMessage.objects.select_related(
+            "author", "author__profile"
+        ).aget(id=message_id)
         context = {"message": message, "user": self.user}
-
-        html = await sync_to_async(render_to_string)(
+        html = render_to_string(
             "chat/partials/chat_message_p.html", context=context
         )
         await self.send(text_data=html)
