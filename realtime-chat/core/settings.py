686375bdc55a2014681fece775daad81b9ef9066
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-05-27 08:17:46 +0300

    update requirements and channel_layers

--- realtime-chat/core/settings.py
+++ realtime-chat/core/settings.py
@@ -69,10 +69,20 @@ TEMPLATES = [
 ASGI_APPLICATION = "core.asgi.application"
 
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
+
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 
